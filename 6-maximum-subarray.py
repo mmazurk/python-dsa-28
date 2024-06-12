cb7c3f62b41e12580ca1b73f8e0137e1344509db
markmazurk <markmazurk@gmail.com>
2024-06-12 12:01:30 -0700

    work on sequencing problems

---
+++ 6-maximum-subarray.py
@@ -0,0 +1,27 @@
+class Solution:
+    """Leetcode hell"""
+
+# This was really hard for me to wrap my brain around because it's new
+# This method keeps track of two things:
+# A global sum and current sum
+# If the current sum goes below zero (e.g., we've had a series of negative numbers)
+# It resets to zero and starts over again
+# This is called Kadane's Algorithm (shit, it has a name)
+
+    def maxSubArray(self, nums):
+        """Leetcode"""
+
+        global_sum = nums[0]
+        current_sum = 0
+
+        for number in nums:
+            if current_sum < 0:
+                current_sum = 0
+            current_sum += number
+            global_sum = max(global_sum, current_sum)
+
+        return global_sum
+
+
+solution = Solution()
+print(solution.maxSubArray([-1, -2, -3, -4]))
---
+++ 7-product-of-array-not-self.py
@@ -0,0 +1,55 @@
+class Solution:
+    def productArray(self, nums):
+
+        # This is how you create a list with a certain amount of members
+        # Here you are making a list that is len(nums) long and filled with 1
+
+        answer = [1] * (len(nums))
+
+        # This problem has a math trick that involves calculating
+        # all the numbers multiplied in order, from the start
+        # and all the numbers multiplied in order, backwards from the end
+        # Why?
+        # if you know this, you can pinpoint any part of the array
+        # and you can multiply the before (prefix) by the after (posfix)
+        # to get the multiple without using division
+        # [1, 2, 3, 4]
+        # For example, you want 1 x 2 x 4 for the 3 slot
+        # So you figure out what 1 x 2 is (prefix)
+        # And you multiply that by 4 x 1 (postfix)
+        # Even worse, you store them in an answer array so you don't take up any more space
+
+        prefix = 1
+        for i in range(len(nums)):
+            answer[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums) - 1, - 1, - 1):
+            answer[i] *= postfix
+            postfix *= nums[i]
+
+        return answer
+
+
+class Brute:
+    def productArray(self, nums):
+        if len(nums) < 3:
+            return None
+
+        answer = []
+
+        for number in nums:
+            product_nums = [*nums]
+            product_nums.remove(number)
+
+            product = 1
+            for number in product_nums:
+                product *= number
+            answer.append(product)
+
+        return answer
+
+
+solution = Solution()
+print(solution.productArray([1, 2, 3, 4]))
